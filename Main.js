8ea8ecc3eceee1d6a4ea8ac2366a6ec1bc332914
Isaiah Quintanilla <qi.filmscreative@gmail.com>
2025-07-01 16:10:37 -0700

    changed module type

--- Main.js
+++ Main.js
@@ -1,2 +1,15 @@
-import 'dotenv/';
+import 'dotenv/config';
 import OpenAI from "openai";
+
+
+const AI_KEY = process.env.AI_KEY;
+const client = new OpenAI({
+    apiKey:AI_KEY,
+});
+
+const response = await client.responses.create({
+    model: "gpt-4.1",
+    input: "Generate a random proffesional email using the gmail suffix and just include the email text in the output",
+});
+
+console.log(response.output_text);
